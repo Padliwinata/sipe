3b39f297c2288d9111d4da12143c908b287e5dad
Padliwinata <rpadliwinata@gmail.com>
2024-08-21 15:04:40 +0700

    initial

---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import random
+
+# Set page config
+st.set_page_config(page_title="SELAMAT LULUS SIDANG SIPE", page_icon="🎓", layout="wide")
+
+# Custom CSS to make it more festive
+st.markdown("""
+    <style>
+    .main {
+        background-color: #f0f8ff;
+    }
+    h1, h2 {
+        color: #4b0082;
+    }
+    .stButton>button {
+        color: #4b0082;
+        background-color: #ff69b4;
+        border-radius: 20px;
+    }
+    p {
+        color: #242424;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Main title
+st.title("🎉 AKHIRNYAAA SIDANGGG CONGRATSSS SIPEEEE! 🎓")
+
+# Name input
+# name = st.text_input("Enter your name:")
+name = "Syifa Hadiarti Aulia"
+
+st.header(f"Hip Hip Hooray, {name}! 🥳")
+
+# Personalized message
+st.write(f"Makasih yaaa udah berjuang sejauh ini 🥹. Aku tau perjalanannya berat bangettt bisa sampe titik ini :')")
+st.write("Kamu udah super keren bisa bertahan sampe akhir di tengah semua hal yang pusing pusing ituu.")
+
+# Interactive elements
+if st.button("Click for a Special Message"):
+    messages = [
+        "KEREN BANGETTTT 🌟",
+        "KAMU HEBATTT 😋",
+        "SELAMATTT YEAAYYY AKHIRNYA BERES SIDANG 🥳",
+        "I'm so proud of you 🥹",
+        "Semoga kamu tetap bersinar dan akan selalu bersinar sipe ❤️"
+    ]
+    st.success(random.choice(messages))
+
+# Fireworks animation
+st.balloons()
+
+# Accomplishment reflection
+accomplishment = st.text_area("Reflect on your journey. What are you most proud of?")
+if accomplishment:
+    st.write("That's amazing! Your hard work truly paid off.")
+
+# Future plans
+st.subheader("Abis beres sidang kamu mau ngapainnn " + name + "? Wkwkwkwk")
+options = st.multiselect(
+    'Pilihan mau ngapainn:',
+    ['Hiling', 'Hibernasi', 'Mukbang', 'Gawe', 'Gatau dah awokwokwok']
+)
+
+if options:
+    st.write("Niceee apapun pilihan kamuuu, selamat yaa udah selesai sidang :) Selamat narik nafas sejenak hehe")
+
+# Celebration song
+audio_file = open('good_time.ogg', 'rb')
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format='audio/ogg')
+
+# Final message
+st.markdown("### 🎊 Once again, congratulations on this incredible achievement! 🎊")
+
+
+# Footer
+st.markdown("---")
+st.markdown("Dengan ❤️untuk sipe!")
